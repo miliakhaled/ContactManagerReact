29ae218ed47d6d6bdbf3fb1c3fb2f4d29e10565d
Khaled Milia <miliakhaled@gmail.com>
2018-09-03 20:45:58 +0100

    fixing deploy problem editing about page

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -5,6 +5,10 @@ const about = props => (
   <div>
     <h1 className="display-4">About</h1>
     <p>A simple contact manager application using React js</p>
+    <p>
+      This app is created by <span className="text-danger">Milia Khaled</span> under TraversyMedia Course
+    </p>
+    <p>React Front to Back</p>
     <div className="text-muted">Version 1.0.0</div>
   </div>
 );
